bb433ff8ea8ee07111728804d218f4746cea6aa2
hmdrawan <hmdrawan@hotmail.com>
2025-05-21 15:25:56 +0300

    first commit

---
+++ src/main/java/com/example/demo/Design.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+
+public class Design {
+    private String name;
+    private int price;
+
+    public Design(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
